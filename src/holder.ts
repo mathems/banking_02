b9cba77c370415637e685c4ceafeff02cc469314
mathems <matthewmoins@gmail.com>
2022-12-16 12:10:44 +1100

    Holder: .getBalance

--- src/holder.ts
+++ src/holder.ts
@@ -1,4 +1,4 @@
-import { MAX_ACCOUNT_BALANCE } from "./types/constants";
+import { MAX_ACCOUNT_BALANCE, MIN_ACCOUNT_BALANCE } from "./types/constants";
 import { generateId } from "./utils/generate-id.util";
 
 
@@ -24,10 +24,22 @@ export class Holder {
 
   public deposit(amount: number) {
     if (MAX_ACCOUNT_BALANCE > (amount + this.balance)) {
-      throw new Error(`The max amount of balance of account is ${MAX_ACCOUNT_BALANCE}!`)
+      throw new Error(`The max amount of balance for account is ${MAX_ACCOUNT_BALANCE}!`)
     }
 
     return this.balance += amount
+  }
+
+  public withdraw(amount: number) {
+    if (MIN_ACCOUNT_BALANCE > (this.balance - amount)) {
+      throw new Error(`The min amount of balance for account is ${MIN_ACCOUNT_BALANCE}!`)
+    }
+
+    return this.balance -= amount
+  }
+
+  public getBalance() {
+    return this.balance;
   }
 
   public getFullName() {
