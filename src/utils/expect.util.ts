cdd6553e1e4b122becacfdef0860d9552325e2d1
mathems <matthewmoins@gmail.com>
2022-12-16 13:11:26 +1100

    expect util upd

--- src/utils/expect.util.ts
+++ src/utils/expect.util.ts
@@ -1,3 +1,12 @@
 export function expectToBe(actual: any, expected: any) {
   return actual === expected;
+  const result = actual === expected;
+
+  if (result) {
+    console.info('good:', actual, '=', expected);
+  } else {
+    console.warn('oops:', actual, '!=', expected);
+  }
+
+  return result;
 }
