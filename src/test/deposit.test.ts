2d51925acd79d7b79f7ba9a58c35a746a346ab56
mathems <matthewmoins@gmail.com>
2022-12-16 13:34:48 +1100

    fix error message

--- src/test/deposit.test.ts
+++ src/test/deposit.test.ts
@@ -1,3 +1,8 @@
+
+// Deposit
+// ● Input: Deposit 1001 500
+// Output: 500
+// ● Input: Deposit 1001 1000
 // Output: 1500
 // ● Input: Deposit 1001 100
 // Output: Minimum deposit amount is 500
@@ -7,9 +12,10 @@
 // Output: 11500
 // ● Input: Deposit 1001 10000
 // Output: Only 3 deposits are allowed in a day
-//
-import { getHolderById } from "../all-holders";
+
+
 import { Bank } from "../bank";
+
 describe('Deposit', () => {
   beforeAll(() => {
     Bank.create('Biba');
@@ -22,7 +28,6 @@ describe('Deposit', () => {
     { id: 1001, amount: 50_000, output: 'Maximum deposit amount is 50000' },
     { id: 1001, amount: 10_000, output: 11_500 },
     { id: 1001, amount: 10_000, output: 'Only 3 deposits are allowed in a day' },
-
   ])('After making deposit ($amount\$) to holder (No."$id") output should be - $output', ({ id, amount, output }) => {
     expect(Bank.deposit(id, amount)).toBe(output);
   });
--- src/types/error-message.enum.ts
+++ src/types/error-message.enum.ts
@@ -1,4 +1,3 @@
-
 export enum ErrorMessage {
   BIG_ACCOUNT_BALANCE = 'Account balance cannot exceed $100,000',
   SMALL_ACCOUNT_BALANCE = 'Account balance cannot be less than $0',
@@ -6,7 +5,7 @@ export enum ErrorMessage {
   BIG_DEPOSIT = 'The maximum deposit amount is $50,000 per transaction',
   SMALL_WITHDRAW = 'The minimum withdrawal amount is $1,000 per transaction',
   BIG_WITHDRAW = 'The maximum withdrawal amount is $25,000 per transaction',
-  MANY_DEPOSITS_FOR_DAY = 'No more than 3 deposits are allowed in a day',
+  MANY_DEPOSITS_FOR_DAY = 'Only 3 deposits are allowed in a day',
   MANY_WITHDRAWS_FOR_DAY = 'No more than 3 withdrawals are allowed in a day',
   HOLDER_NOT_FOUNT = 'Account number entered during deposit or withdrawal should be valid',
   NOT_SUFFICIENT_BALANCE = 'Account has not sufficient balance during withdrawals',
