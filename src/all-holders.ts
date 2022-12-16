e09147f80a3ce58fed8a3f40c21e141c550aea6d
mathems <matthewmoins@gmail.com>
2022-12-16 12:34:00 +1100

    use erors

--- src/all-holders.ts
+++ src/all-holders.ts
@@ -1,12 +1,13 @@
 import { Holder } from "./holder";
+import { ErrorMessage } from "./types/error-message.enum";
 
 export const ALL_HOLDERS = new Map<number, Holder>();
 
 export function getHolderById(id: number) {
   const holder = ALL_HOLDERS.get(id);
 
-  if(!holder) {
-    throw new Error(`There aren't any holders with such id (${id})!`);
+  if (!holder) {
+    throw new Error(ErrorMessage.HOLDER_NOT_FOUNT);
   }
 
   return holder;
