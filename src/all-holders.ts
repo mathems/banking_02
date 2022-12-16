73c5b104ef4375a6d6d19c97b4ada52134d04a60
mathems <matthewmoins@gmail.com>
2022-12-16 12:25:25 +1100

    upd allHolders with getHolderByID

--- src/all-holders.ts
+++ src/all-holders.ts
@@ -1,3 +1,13 @@
 import { Holder } from "./holder";
 
 export const ALL_HOLDERS = new Map<number, Holder>();
+
+export function getHolderById(id: number) {
+  const holder = ALL_HOLDERS.get(id);
+
+  if(!holder) {
+    throw new Error(`There aren't any holders with such id (${id})!`);
+  }
+
+  return holder;
+}
