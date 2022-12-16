270e337ccbbf5fa0ea5590532a77f9771152d608
mathems <matthewmoins@gmail.com>
2022-12-16 12:49:16 +1100

    fix COMMAN_EXECUTER

--- src/bank.ts
+++ src/bank.ts
@@ -8,19 +8,19 @@ const COMMAND_EXECUTOR = {
   [InputCommand.CREATE](fullName: string) {
     const id = generateId();
     const holder = Holder.create(fullName, id);
-
     ALL_HOLDERS.set(id, holder);
-
     return id;
   },
   [InputCommand.DEPOSIT]: (id: number, amount: number) => getHolderById(id).deposit(amount),
   [InputCommand.WITHDRAW]: (id: number, amount: number) => getHolderById(id).withdraw(amount),
   [InputCommand.BALANCE]: (id: number) => getHolderById(id).getBalance(),
-}
+} as const;
+function transfer(sourceAccountId: number, targetAccountId: number, amount: number) {
+  const sender = getHolderById(sourceAccountId);
+  const receiver = getHolderById(targetAccountId);
 
+  export class Bank {
+    public static execute(input: InputCommand) {
 
-export class Bank {
-  public static execute(input: InputCommand) {
-
+    }
   }
-}
