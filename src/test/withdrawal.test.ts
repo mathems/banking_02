186c3e0032f8ad1e8e1588c423b99889a6b24b5c
mathems <matthewmoins@gmail.com>
2022-12-16 13:42:37 +1100

    withdrawal test

---
+++ src/test/withdrawal.test.ts
@@ -0,0 +1,23 @@
+
+// Withdrawal
+// ● Input: Withdraw 1001 500
+// Output: Minimum withdrawal amount is 1000
+// ● Input: Withdraw 1001 20000
+// Output: Insufficient balance
+// ● Input: Withdraw 1001 1000
+// Output: 10500
+// ● Input: Withdraw 1001 1900
+// Output: 8600
+// ● Input: Withdraw 1001 1000
+// Output: 7600
+// ● Input: Withdraw 1001 5000
+// Output: Only 3 withdrawals are allowed in a day
+
+
+describe('Withdrawal', () => {
+  beforeAll(() => {
+
+  });
+
+  it('where is any test???', () => { });
+});
