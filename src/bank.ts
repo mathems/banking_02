d74a4ad4e3736a41d6d9475b46584e0f9fdb0b86
mathems <matthewmoins@gmail.com>
2022-12-16 13:02:00 +1100

    minimal functional is ready

--- src/bank.ts
+++ src/bank.ts
@@ -1,5 +1,6 @@
 import { ALL_HOLDERS, getHolderById } from "./all-holders";
 import { Holder } from "./holder";
+import { ErrorMessage } from "./types/error-message.enum";
 import { InputCommand } from "./types/input_command.enum";
 import { generateId } from "./utils/generate-id.util";
 
@@ -11,17 +12,55 @@ const COMMAND_EXECUTOR = {
     ALL_HOLDERS.set(id, holder);
     return id;
   },
-  [InputCommand.DEPOSIT]: (id: number, amount: number) => getHolderById(id).deposit(amount),
-  [InputCommand.WITHDRAW]: (id: number, amount: number) => getHolderById(id).withdraw(amount),
+
+  [InputCommand.DEPOSIT]: (id: number, amount: number) => {
+    const holder = getHolderById(id);
+    const checkResult = holder.checkIsDepositPossible(amount);
+
+    if (Number.isNaN(checkResult)) {
+      throw new Error(checkResult as ErrorMessage);
+    }
+
+    return holder.deposit(amount, 'I should to call check* method before this operation');
+  },
+
+  [InputCommand.WITHDRAW]: (id: number, amount: number) => {
+    const holder = getHolderById(id);
+    const checkResult = holder.checkIsWithdrawPossible(amount);
+
+    if (Number.isNaN(checkResult)) {
+      throw new Error(checkResult as ErrorMessage);
+    }
+
+    return holder.withdraw(amount, 'I should to call check* method before this operation');
+  },
+
   [InputCommand.BALANCE]: (id: number) => getHolderById(id).getBalance(),
 } as const;
+
+
 function transfer(sourceAccountId: number, targetAccountId: number, amount: number) {
   const sender = getHolderById(sourceAccountId);
   const receiver = getHolderById(targetAccountId);
+  const senderCheckResult = sender.checkIsWithdrawPossible(amount);
+  const receiverCheckResult = receiver.checkIsDepositPossible(amount);
+
+  if ([senderCheckResult, receiverCheckResult].some((checkResult) => Number.isNaN(checkResult))) {
+    return 'failure';
+  }
+
+  sender.withdraw(amount, 'I should to call check* method before this operation');
+  receiver.deposit(amount, 'I should to call check* method before this operation');
+
+  return 'successful';
 }
 
 export class Bank {
   public static execute(input: InputCommand) {
-
+    if (input === InputCommand.TRANSFER) {
+      return transfer;
+    } else {
+      COMMAND_EXECUTOR[input];
+    }
   }
 }
