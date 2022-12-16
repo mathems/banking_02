56e133ccccc77a009e680b9bd7e14247ca6787aa
mathems <matthewmoins@gmail.com>
2022-12-16 13:19:40 +1100

    test

---
+++ src/test/account-creation.test.ts
@@ -0,0 +1,19 @@
+
+/** 
+Account creation
+● Input: Create “Steve Rogers”
+Output: 1001
+● Input: Create “Diana Prince”
+Output: 1002
+*/
+
+import { Bank } from "../bank";
+
+describe('Account creation', () => {
+  it.each([
+    { name: 'Steve Rogers', id: 1001 },
+    { name: 'Diana Price', id: 1002 },
+  ])('Should create holder "$name" with "id" $id', ({ name, id }) => {
+    expect(Bank.create(name)).toBe(id);
+  });
+});
