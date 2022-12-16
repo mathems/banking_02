734056e0afc2f92609c8aec0a08521064e4d26d8
mathems <matthewmoins@gmail.com>
2022-12-16 12:20:28 +1100

    COMMAND_EXECUTOR

--- src/bank.ts
+++ src/bank.ts
@@ -1,7 +1,21 @@
+import { Holder } from "./holder";
 import { InputCommand } from "./types/input_command.enum";
+import { generateId } from "./utils/generate-id.util";
 
+
+const ALL_HOLDERS = new Map<number, Holder>();
+const COMMAND_EXECUTOR = {
+  [InputCommand.CREATE](fullName: string) {
+    const id = generateId();
+    const holder = Holder.create(fullName, id);
+
+    ALL_HOLDERS.set(id, holder);
+
+    return id;
+  }
+}
 export class Bank {
-  public static execute(input: InputCommand){
-    
+  public static execute(input: InputCommand) {
+
   }
 }
